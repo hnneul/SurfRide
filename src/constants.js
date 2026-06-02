9d71264dbf35e825fc7fb50fc55d9814b7613efd
김하늘 <>
2026-06-02 22:25:37 +0900

    refactor: 공통 상수 constants.js로 통합

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,16 @@
-// 논리 해상도
 export const LOGICAL_WIDTH  = 1920;
 export const LOGICAL_HEIGHT = 1080;
 
-// 게임 상태
-export const GameState = Object.freeze({
-  MAIN:     'MAIN',
-  WORLDMAP: 'WORLDMAP',
-  PLAYING:  'PLAYING',
-  PAUSED:   'PAUSED',
-  RESULT:   'RESULT',
+export const Lane = Object.freeze({ TOP: 0, MID: 1, BOT: 2 });
+
+export const LANE_Y = Object.freeze({
+  [0]: LOGICAL_HEIGHT * 0.30,
+  [1]: LOGICAL_HEIGHT * 0.55,
+  [2]: LOGICAL_HEIGHT * 0.78,
+});
+
+export const LANE_MULTIPLIER = Object.freeze({
+  [0]: 1.0,
+  [1]: 1.2,
+  [2]: 1.5,
 });
